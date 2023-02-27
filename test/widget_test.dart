5b5b7ce43f055ff239fdaaffa085d6af58bda71c
ritikprajapat <ritikprajapat.04@gmail.com>
2023-02-27 19:31:03 +0530

    Update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:meals_app/app.dart';
+import 'package:meal_app/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
